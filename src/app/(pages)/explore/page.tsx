0b218b6c8f8884cc00e2619a8ed091de85dc4b8e
Gaurav Mehla <gaurav@mehla.in>
2023-11-08 13:44:08 -0800

    google maps integrated with pins

--- src/app/(pages)/explore/page.tsx
+++ src/app/(pages)/explore/page.tsx
@@ -3,309 +3,65 @@
 import React, { useEffect, useState } from "react";
 // import { useRouter } from "next/router";
 import { UserAuth } from "../../context/AuthContext.js";
-import firebase from "firebase/app";
-import "firebase/database";
+import { RecenterControl } from "@/app/components/RecenterControl";
+import { darkMapStyle } from "./mapStyles";
+import { db, storage } from "@/app/firebase";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const postsInKm = 5;
+
+// Function to calculate distance between two locations
+function getDistanceFromLatLonInKm(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c; // Distance in km
+}
+
+function deg2rad(deg: number): number {
+  return deg * (Math.PI / 180);
+}
 
 const page = () => {
   const { user } = UserAuth();
   const [userLocation, setUserLocation] = useState({
-    lat: 0,
-    lng: 0,
+    lat: 49.273282,
+    lng: -123.104274,
   });
   // const router = useRouter();
 
   const [map, setMap] = useState<google.maps.Map | null>(null); // Store the map instance
   const [isUserLocationAvailable, setIsUserLocationAvailable] = useState(false);
 
-  // https://mapstyle.withgoogle.com/
-  const darkMapStyle = [
-    {
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#8ec3b9",
-        },
-      ],
-    },
-    {
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1a3646",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.country",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#4b6878",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "labels",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#64779e",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.province",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#4b6878",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#334e87",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.natural",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#283d6a",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#6f9ba5",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "poi.business",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "labels.text",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#3C7680",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#304a7d",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#98a5be",
-        },
-      ],
-    },
-    {
-      featureType: "road",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#2c6675",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#255763",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#b0d5ce",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#023e58",
-        },
-      ],
-    },
-    {
-      featureType: "road.local",
-      elementType: "labels",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "transit",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#98a5be",
-        },
-      ],
-    },
-    {
-      featureType: "transit",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          color: "#1d2c4d",
-        },
-      ],
-    },
-    {
-      featureType: "transit.line",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          color: "#283d6a",
-        },
-      ],
-    },
-    {
-      featureType: "transit.station",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#3a4762",
-        },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "geometry",
-      stylers: [
-        {
-          color: "#0e1626",
-        },
-      ],
-    },
-    {
-      featureType: "water",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          color: "#4e6d70",
-        },
-      ],
-    },
-  ];
-
-  const pins = [
-    { latitude: 37.7749, longitude: -122.4194 }, // Example pin 1
-    { latitude: 34.0522, longitude: -118.2437 }, // Example pin 2
-    // Add more hardcoded pins as needed
-  ];
+  const [selectedPin, setSelectedPin] = useState<Post | null>(null);
 
   const mapContainerStyle = {
     width: "100%", // Set the width to 100% of the viewport
     height: "93vh", // Set the height to 100% of the viewport height
   };
+
+  interface Post {
+    user: string;
+    title: string;
+    coords: {
+      lat: number;
+      lng: number;
+    };
+  }
+
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     const mapElement = document.getElementById("map");
@@ -354,56 +110,52 @@ const page = () => {
         });
       }
 
-      class RecenterControl {
-        constructor(controlDiv: HTMLDivElement, map: google.maps.Map) {
-          // Create the control button
-          const controlUI = document.createElement("div");
-          controlUI.style.backgroundColor = "#fff";
-          controlUI.style.border = "1px solid #ccc";
-          controlUI.style.borderRadius = "3px";
-          controlUI.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.3)";
-          controlUI.style.cursor = "pointer";
-          controlUI.style.marginBottom = "10px";
-          controlUI.style.textAlign = "center";
-          controlUI.title = "Click to recenter the map";
-          controlDiv.appendChild(controlUI);
-
-          // Set the button's text and click event
-          const controlText = document.createElement("div");
-          controlText.style.color = "rgb(25,25,25)";
-          controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-          controlText.style.fontSize = "16px";
-          controlText.style.lineHeight = "38px";
-          controlText.style.paddingLeft = "5px";
-          controlText.style.paddingRight = "5px";
-          controlText.innerHTML = "Recenter";
-          controlUI.appendChild(controlText);
-
-          // Add a click event to recenter the map on the user's location
-          controlUI.addEventListener("click", () => {
-            map.setCenter(userLocation);
-          });
-        }
-      }
-
       // Create a custom control for recentering
       const recenterControlDiv = document.createElement("div");
-      const recenterControl = new RecenterControl(recenterControlDiv, map);
+      const recenterControl = new RecenterControl(
+        recenterControlDiv,
+        map,
+        userLocation
+      );
 
       // Set the position for the recenter control above the Street View control
       map.controls[google.maps.ControlPosition.TOP_CENTER].push(
         recenterControlDiv
       );
 
+      let openInfoWindow: google.maps.InfoWindow | null = null;
+
       // Add markers based on the 'pins' array
-      pins.forEach((pin) => {
-        new google.maps.Marker({
-          position: { lat: pin.latitude, lng: pin.longitude },
+      posts.forEach((pin) => {
+        const marker = new google.maps.Marker({
+          position: { lat: pin.coords.lat, lng: pin.coords.lng },
           map: map,
+        });
+
+        marker.addListener("click", () => {
+          // Close the currently open InfoWindow if it exists
+          if (openInfoWindow) {
+            openInfoWindow.close();
+          }
+
+          const infowindow = new google.maps.InfoWindow({
+            content: `
+              <div>
+                <h2>${pin.title}</h2>
+                <p>Posted by: ${pin.user}</p>
+                <p>Coordinates: ${pin.coords.lat}, ${pin.coords.lng}</p>
+              </div>
+            `,
+          });
+
+          infowindow.open(map, marker);
+
+          // Set the new InfoWindow as the currently open one
+          openInfoWindow = infowindow;
         });
       });
     }
-  }, [userLocation, pins, isUserLocationAvailable]);
+  }, [userLocation, isUserLocationAvailable, posts]);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -427,6 +179,31 @@ const page = () => {
       console.error("Geolocation is not supported");
     }
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = getFirestore();
+      const postsRef = collection(db, "posts"); // replace 'posts' with your collection name
+      const querySnapshot = await getDocs(postsRef);
+      const nearbyPosts: Post[] = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        const distanceInKm = getDistanceFromLatLonInKm(
+          userLocation.lat,
+          userLocation.lng,
+          data.coords.lat,
+          data.coords.lng
+        );
+        if (distanceInKm <= postsInKm) {
+          nearbyPosts.push(data as Post);
+        }
+      });
+
+      setPosts(nearbyPosts);
+    };
+
+    fetchData();
+  }, [userLocation]);
 
   return (
     <div className="flex flex-1 items-center justify-center">
--- src/app/components/RecenterControl.tsx
+++ src/app/components/RecenterControl.tsx
@@ -1,31 +1,36 @@
-export default class RecenterControl {
-    constructor(controlDiv: HTMLDivElement, map: google.maps.Map, userLocation: google.maps.LatLng) {
-      // Create the control button
-      const controlUI = document.createElement("div");
-      controlUI.style.backgroundColor = "#fff";
-      controlUI.style.border = "1px solid #ccc";
-      controlUI.style.borderRadius = "3px";
-      controlUI.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.3)";
-      controlUI.style.cursor = "pointer";
-      controlUI.style.marginBottom = "10px";
-      controlUI.style.textAlign = "center";
-      controlUI.title = "Click to recenter the map";
-      controlDiv.appendChild(controlUI);
+export class RecenterControl {
+  constructor(
+    controlDiv: HTMLDivElement,
+    map: google.maps.Map,
+    userLocation: google.maps.LatLngLiteral
+  ) {
+    // Create the control button
+    const controlUI = document.createElement("div");
+    controlUI.style.backgroundColor = "#fff";
+    controlUI.style.marginRight = "0px";
+    controlUI.style.margin = "20px";
+    controlUI.style.border = "1px solid #ccc";
+    controlUI.style.borderRadius = "3px";
+    controlUI.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.3)";
+    controlUI.style.cursor = "pointer";
+    controlUI.style.textAlign = "center";
+    controlUI.title = "Click to recenter the map";
+    controlDiv.appendChild(controlUI);
 
-      // Set the button's text and click event
-      const controlText = document.createElement("div");
-      controlText.style.color = "rgb(25,25,25)";
-      controlText.style.fontFamily = "Roboto,Arial,sans-serif";
-      controlText.style.fontSize = "16px";
-      controlText.style.lineHeight = "38px";
-      controlText.style.paddingLeft = "5px";
-      controlText.style.paddingRight = "5px";
-      controlText.innerHTML = "Recenter";
-      controlUI.appendChild(controlText);
+    // Set the button's text and click event
+    const controlText = document.createElement("div");
+    controlText.style.color = "rgb(25,25,25)";
+    controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+    controlText.style.fontSize = "16px";
+    controlText.style.lineHeight = "38px";
+    controlText.style.paddingLeft = "5px";
+    controlText.style.paddingRight = "5px";
+    controlText.innerHTML = "Recenter";
+    controlUI.appendChild(controlText);
 
-      // Add a click event to recenter the map on the user's location
-      controlUI.addEventListener("click", () => {
-        map.setCenter(userLocation);
-      });
-    }
+    // Add a click event to recenter the map on the user's location
+    controlUI.addEventListener("click", () => {
+      map.setCenter(userLocation);
+    });
   }
+}
