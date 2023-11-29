2e692a0d7cbe9e0e7d9ed9505eb63a81651c0624
Gaurav Mehla <gaurav@mehla.in>
2023-11-29 14:23:31 -0800

    final fixes

--- src/app/components/Posts/Post.tsx
+++ src/app/components/Posts/Post.tsx
@@ -117,61 +117,39 @@ const Post: React.FC<Props> = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-black mx-auto max-w-screen-lg px-8 pt-5 pb-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-black mx-auto max-w-screen-lg px-8 pt-5 pb-3">
       {userPosts.map((post, index) => (
         <div
           key={index}
-          className="bg-gray-500 shadow-md p-6 transition-transform transform hover:scale-105 border border-solid border-gray-500 ring-2 ring-gray-500 ring-opacity-50 rounded-md"
+          className="bg-white shadow-md rounded-md overflow-hidden relative"
         >
           <button
             onClick={() => post.postId && handleDelete(post.postId)}
-            type="button"
-            className="text-gray-400 bg-gray-600 hover:bg-gray-200 float-right hover:text-gray-900 rounded-sm text-sm p-1.5 ml-auto inline-flex items-center"
+            className="bg-gray-200 text-gray-700 p-2 absolute"
+            style={{ zIndex: 10 }}
           >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            &times;
           </button>
-          <h1
-            className="text-2xl font-normal mb-2 pl-2 bg-gray-600 rounded-sm text-gray-300"
-            style={{ height: "32px", overflowY: "auto" }}
-          >
-            {post.title}
-          </h1>
-          <p className="pl-2 text-gray-900">Description</p>
-          <div
-            className="bg-gray-600 rounded-sm p-2 mb-4 mix-blend-blemish"
-            style={{ height: "100px", overflowY: "auto" }}
-          >
-            <p className="text-gray-400">{post.description}</p>
-          </div>
+
+          {/* Image */}
           {post.images && post.images.length > 0 && (
             <div className="relative">
               <img
-                className="w-full h-60 object-cover rounded-sm"
+                className="w-full h-60 object-cover rounded-t-md"
                 src={post.images[currentImages[index]]}
                 alt={`Image ${currentImages[index]}`}
               />
               {post.images.length > 1 && (
-                <div>
+                <div className="absolute top-0 right-0 flex items-center space-x-2">
                   <button
                     onClick={() => prevImage(index)}
-                    className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-300 text-black p-2 rounded-full"
+                    className="bg-gray-200 text-gray-700 p-2"
                   >
                     &lt;
                   </button>
                   <button
                     onClick={() => nextImage(index)}
-                    className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-300 text-black p-2 rounded-full"
+                    className="bg-gray-200 text-gray-700 p-2"
                   >
                     &gt;
                   </button>
@@ -179,34 +157,35 @@ const Post: React.FC<Props> = () => {
               )}
             </div>
           )}
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-            className="bg-gray-600 rounded-sm text-gray-400 p-1 mt-4"
-          >
-            <svg
-              height="15pt"
-              viewBox="0 0 512 512"
-              width="15pt"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#000000"
-            >
-              <path d="M256 0C156.125 0 76 80.125 76 180c0 39.625 13.875 67.5 38 112.25l141.875 219.75c2.125 3.25 6.125 5.5 10.125 5.5s8-2.25 10.125-5.5L398 292.25C422.125 247.5 436 219.625 436 180 436 80.125 355.875 0 256 0zm0 282.25c-31.875 0-57.5-25.625-57.5-57.5s25.625-57.5 57.5-57.5 57.5 25.625 57.5 57.5-25.625 57.5-57.5 57.5z" />
-            </svg>
-            <p style={{ marginLeft: "5px", height: "50px", overflowY: "auto" }}>
-              {post.location}
+          {/* Content */}
+          <div className="p-4">
+            {/* Title */}
+            <h1 className="text-xl font-semibold mb-2 text-gray-900">
+              {post.title}
+            </h1>
+
+            {/* Description */}
+            <p className="text-gray-700 mb-4">{post.description}</p>
+
+            {/* Location */}
+            <div className="flex items-center text-gray-600">
+              <svg
+                height="15pt"
+                viewBox="0 0 24 24"
+                width="15pt"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#000000"
+              >
+                <path d="M18,4.48a8.45,8.45,0,0,0-12,12l5.27,5.28a1,1,0,0,0,1.42,0L18,16.43A8.45,8.45,0,0,0,18,4.48ZM16.57,15,12,19.59,7.43,15a6.46,6.46,0,1,1,9.14,0ZM9,7.41a4.32,4.32,0,0,0,0,6.1,4.31,4.31,0,0,0,7.36-3,4.24,4.24,0,0,0-1.26-3.05A4.3,4.3,0,0,0,9,7.41Zm4.69,4.68a2.33,2.33,0,1,1,.67-1.63A2.33,2.33,0,0,1,13.64,12.09Z" />
+              </svg>
+              <p className="ml-2 text-sm">{post.location}</p>
+            </div>
+
+            {/* Price */}
+            <p className="text-green-900 font-semibold text-lg mt-4">
+              ${post.price}
             </p>
           </div>
-          <p
-            className="text-green-900 font-semibold pt-4 pl-1"
-            style={{
-              fontSize: "1.2rem",
-            }}
-          >
-            ${post.price}
-          </p>
         </div>
       ))}
     </div>
